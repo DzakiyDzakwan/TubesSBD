add1b7ab93a7e23560e37e9223bb496672e27046
Dzakiy Dzakwan <dzakcart@gmail.com>
2022-04-18 20:57:44 +0700

    commit jaki

--- elearning/routes/web.php
+++ elearning/routes/web.php
@@ -19,26 +19,6 @@ use App\Http\Controllers\AdminController;
     return view('dashboardadmin',compact('page'));
 }); */
 
-/* Route::get('/admin/user', function() {
-    $page = 'user';
-    return view('admin.user', compact('page'));
-}); */
-
-/* Route::get('/admin/mahasiswa', function() {
-    $page = "mahasiswa";
-    return view('admin.mahasiswa', compact('page'));
-}); */
-
-/* Route::get('/admin/dosen', function() {
-    $page = 'dosen';
-    return view('admin.dosen', compact('page'));
-}); */
-
-/* Route::get('/admin/faculty', function() {
-    $page = 'faculty';
-    return view('admin.faculty', compact('page'));
-}); */
-
 Route::get('/admin', [AdminController::class, 'dashboard']);
 
 Route::get('/admin/user', [AdminController::class, 'user']);
