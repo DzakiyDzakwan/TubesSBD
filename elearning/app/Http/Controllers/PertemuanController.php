1fb46951aa0b4e2dfcee41c26e8d4403b8123ebb
Neha Sabila <nehasabila62@gmail.com>
2022-05-20 11:13:08 +0700

    edit, delete pertemuan

--- elearning/app/Http/Controllers/PertemuanController.php
+++ elearning/app/Http/Controllers/PertemuanController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Pertemuan;
+use App\Models\Enrollment;
+use App\Models\Kelas;
 
 
 class PertemuanController extends Controller
@@ -34,5 +36,53 @@ class PertemuanController extends Controller
     // public function pertemuan(){
     //     $pertemuans = pertemuan::join('kelas','pertemuans.kelas','=','kelas.kelas_id')->select('pertemuans.nama_pertemuan','pertemuans.deskripsi','pertemuans.tanggal_pertemuan')->get();
     // }
+
+    public function pertemuanDelete($id) {
+
+        Pertemuan::where('pertemuan_id', $id)->delete();
+
+        return back();
+
+    }
+
+    public function pertemuanEdit($id) {
+        
+        $nik = auth()->user()->NIK;
+
+        //sidebar
+        if(auth()->user()->status === 'mahasiswa'){
+
+
+            $kelas = Enrollment::join('kelas','enrollments.kelas','=','kelas.kelas_id')->join('mata_kuliahs','kelas.mata_kuliah','=','mata_kuliahs.kode_mata_kuliah')->join('dosens', 'kelas.dosen', '=', 'dosens.NIP')->join('users', 'dosens.user' , '=', 'users.NIK')->select('enrollments.kelas','kelas.kelas_id', 'kelas.kelas','mata_kuliahs.nama_matkul', 'users.first_name', 'users.last_name', 'mata_kuliahs.kode_mata_kuliah')->where('enrollments.user',$nik)->get();
+
+        } elseif(auth()->user()->status === 'dosen') {
+
+            $kelas = Kelas::join('dosens', 'kelas.dosen', '=', 'dosens.NIP')->join('users', 'dosens.user', '=', 'users.NIK')->join('mata_kuliahs', 'kelas.mata_kuliah', '=', 'mata_kuliahs.kode_mata_kuliah')->select('mata_kuliahs.nama_matkul', 'mata_kuliahs.kode_mata_kuliah','kelas.kelas_id', 'kelas.kelas', 'users.first_name', 'users.last_name')->where('dosens.user', $nik)->get();
+
+        }
+
+
+        $pertemuan = Pertemuan::join('kelas','pertemuans.kelas','=','kelas.kelas_id')->select('pertemuans.nama_pertemuan','pertemuans.deskripsi','pertemuans.tanggal_pertemuan','pertemuans.pertemuan_id','pertemuans.kelas','kelas.kelas_id')->where('pertemuans.pertemuan_id', $id)->get();
+
+        return view('user.editpertemuan', [
+            'pertemuan'=>$pertemuan,
+            'enrollmatkul'=>$kelas
+        
+        ]);
+
+    }
+
+    public function pertemuanUpdate(Request $request){
+
+        Pertemuan::where('pertemuan_id', $request->id)->update([
+            
+           'nama_pertemuan'=>$request->nama_pertemuan,
+           'deskripsi'=>$request->deskripsi,
+           'tanggal_pertemuan'=>$request->tanggal_pertemuan
+
+        ]);
+        return back()->with('success', 'Data berhasil diupdate');
+
+    }
 
 }
